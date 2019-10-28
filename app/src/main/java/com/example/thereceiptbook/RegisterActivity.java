7a9534a588fc72d632d95d0b27d750ca577f6132
Enoch Kojo Deladem Viewu <viewuenoch@gmail.com>
2019-10-28 10:36:46 +0000

    Registration of user

--- app/src/main/java/com/example/thereceiptbook/RegisterActivity.java
+++ app/src/main/java/com/example/thereceiptbook/RegisterActivity.java
@@ -1,19 +1,109 @@
 package com.example.thereceiptbook;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.thereceiptbook.VolleyClasses.MySingleton;
+import com.example.thereceiptbook.VolleyClasses.MyVolley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button signUp;
+    private TextInputEditText fullName;
+    private TextInputEditText companyName;
+    private TextInputEditText phoneNumber;
+    private TextInputEditText userPassword;
+    private TextInputEditText confirmUserPassword;
+    private ProgressDialog progressDialog;
+    //private final Class aClass = RegisterActivity.class;
+    private final String pageUrl = "http://192.168.137.1/thereceiptbook/registerUser.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //Set the applicationtoolbar for the registration activity
-        Toolbar toolbar = findViewById(R.id.toolbarNav);
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.titlecolor));
+
+        //Initialize the various fields
+        fullName = findViewById(R.id.full_name);
+        companyName = findViewById(R.id.company_name);
+        phoneNumber = findViewById(R.id.phone_number);
+        userPassword = findViewById(R.id.password);
+        confirmUserPassword = findViewById(R.id.confirm_password);
+        progressDialog = new ProgressDialog(this);
+        signUp = findViewById(R.id.sign_up_button);
+
+
+        //Create an onClick Listener for button
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+    }
+
+
+    private void createUser() {
+
+        progressDialog.setMessage("Registering User ...");
+        progressDialog.show();
+
+        //Save user input in string
+        final String userFullName = fullName.getText().toString().trim();
+        final String userCompanyName = companyName.getText().toString().trim();
+        final String userPhoneNumber = phoneNumber.getText().toString().trim();
+        final String userPasswordValue = userPassword.getText().toString().trim();
+        final String userConfirmedPassword = confirmUserPassword.getText().toString().trim();
+
+        RequestQueue requestQueue = MySingleton.getInstance(getApplicationContext()).
+                getRequestQueue();
+        requestQueue.start();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, pageUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("full_name", userFullName);
+                params.put("company", userCompanyName);
+                params.put("phone_number", userPhoneNumber);
+                params.put("user_pass", userPasswordValue);
+                params.put("confirm_password", userConfirmedPassword);
+                return params;
+            }
+        };
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 }
--- app/src/main/java/com/example/thereceiptbook/SplashScreen.java
+++ app/src/main/java/com/example/thereceiptbook/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.thereceiptbook;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -8,6 +9,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Network;
@@ -18,8 +20,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.StringRequest;
 import com.example.thereceiptbook.VolleyClasses.MySingleton;
+import com.example.thereceiptbook.VolleyClasses.MyVolley;
+
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,12 +33,15 @@ public class SplashScreen extends AppCompatActivity {
 
     private CircleImageView logo;
     private RelativeLayout splashLayout;
-    private String server_url = "http://192.168.2.85/thereceiptbook/Main.php";
+    private String server_url = "http://192.168.137.1/thereceiptbook/Main.php";
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Class aClass = RegisterActivity.class;
 
         logo = findViewById(R.id.know_house_logo);
         splashLayout = findViewById(R.id.splash_layout);
@@ -40,7 +49,7 @@ public class SplashScreen extends AppCompatActivity {
         splashLayout.startAnimation(myAnim);
         logo.startAnimation(myAnim);
 
-        final RequestQueue requestQueue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        /*final RequestQueue requestQueue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
         requestQueue.start();
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
@@ -49,7 +58,7 @@ public class SplashScreen extends AppCompatActivity {
                     public void onResponse(String response) {
                         Toast toast = Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG);
                         toast.show();
-                        Intent intent = new Intent(SplashScreen.this, HomeActivity.class);
+                        Intent intent = new Intent(SplashScreen.this, RegisterActivity.class);
                         startActivity(intent);
                         requestQueue.stop();
                     }
@@ -66,6 +75,17 @@ public class SplashScreen extends AppCompatActivity {
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
         ));
-        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);*/
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                MyVolley myVolley = new MyVolley(SplashScreen.this);
+                myVolley.volleyRequest(server_url,RegisterActivity.class);
+            }
+        },3000);
+
     }
 }
