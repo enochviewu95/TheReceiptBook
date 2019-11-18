61cee2b28b6ea6910c3fbc99db0237cd99a90d01
Enoch Kojo Deladem Viewu <viewuenoch@gmail.com>
2019-11-18 19:44:50 +0000

    Receipt for recycler added

--- app/src/main/java/com/example/thereceiptbook/Constants.java
+++ app/src/main/java/com/example/thereceiptbook/Constants.java
@@ -1,7 +1,7 @@
 package com.example.thereceiptbook;
 
 public class Constants {
-    private static final String ROOT_URL = "http://192.168.43.110/thereceiptbook/";
+    private static final String ROOT_URL = "http://192.168.137.1/thereceiptbook/";
 
     public static final String URL_LOGIN = ROOT_URL+"UserLogin.php";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
